be2bebd9a31926cf1a74e0f5363e97ee114a54c4
aaliyahfry-hue <aaliyah.fry@sarahtreed.org>
2026-09-17 18:00:52 +0000

    to do 2

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -20,27 +20,34 @@ $(function () {
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+    createPlatform(canvas.width, -50, 50, canvas.height + 100) ; // right wall
 
     //////////////////////////////////
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
+createPlatform(500, 0, 20, 290);
+createPlatform(1350, 400, 50, 50, "red");
+createPlatform(100, 20, 10, 10, "lime"); // bright green for a finished platform
 
 
 
 
     // TODO 3 - Create Collectables
+createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
 
 
 
     
     // TODO 4 - Create Cannons
+createCannon("top", 200, 100);
+createCannon("right", 300, 2000);
 
 
     
